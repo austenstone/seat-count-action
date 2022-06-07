58191dc625d2af7b0be6c9b3f680013cc73b0f50
Austen Stone <austenstone@github.com>
2022-06-07 13:55:31 -0400

    test

--- src/run.ts
+++ src/run.ts
@@ -3,11 +3,13 @@ import * as github from '@actions/github';
 
 interface Input {
   token: string;
+  org: string;
 }
 
 export function getInputs(): Input {
   const result = {} as Input;
   result.token = core.getInput('github-token');
+  result.org = core.getInput('org');
   return result;
 }
 
@@ -16,15 +18,9 @@ const run = async (): Promise<void> => {
     const input = getInputs();
     const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
 
-    const {
-      viewer: { login },
-    } = await octokit.graphql(`{ 
-      viewer { 
-        login
-      }
-    }`);
+    const response = await octokit.request(`GET /orgs/${input.org}`)
 
-    core.info(`Hello, ${login}!`);
+    core.info(JSON.stringify(response));
   } catch (error) {
     core.setFailed(error instanceof Error ? error.message : JSON.stringify(error))
   }
