6f7d7a467484366c7792e2f4fed021a1b3763bc5
Austen Stone <austenstone@github.com>
2022-08-17 12:41:57 -0400

    test

--- src/run.ts
+++ src/run.ts
@@ -5,12 +5,14 @@ import { Endpoints } from '@octokit/types';
 interface Input {
   token: string;
   org: string;
+  enterprise: string;
 }
 
 export function getInputs(): Input {
   const result = {} as Input;
   result.token = core.getInput('github-token');
   result.org = core.getInput('org');
+  result.enterprise = core.getInput('enterprise');
   return result;
 }
 
@@ -19,16 +21,28 @@ const run = async (): Promise<void> => {
     const input = getInputs();
     const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
 
-    const orgResponse = await octokit.request(`GET /orgs/${input.org}`);
-    const plan = orgResponse.data.plan;
+    let plan;
+    if (input.enterprise) {
+      const entResponse = await octokit.request(`GET /enterprises/${input.enterprise}/consumed-licenses`);
+      plan = {
+        filled_seats: entResponse.data.total_seats_consumed,
+        seats: entResponse.data.total_seats_purchased
+      };
+    } else if (input.org) {
+      const orgResponse = await octokit.request(`GET /orgs/${input.org}`);
+      plan = orgResponse.data.plan;
+      if (plan) {
+        core.setOutput('name', plan.name);
+        core.setOutput('space', plan.space);
+        core.setOutput('private_repos', plan.private_repos);
+      }
+    } else {
+      throw new Error('No org or enterprise specified');
+    }
 
     if (plan) {
-      core.setOutput('name', plan.name);
-      core.setOutput('space', plan.space);
-      core.setOutput('private_repos', plan.private_repos);
       core.setOutput('filled_seats', plan.filled_seats);
       core.setOutput('seats', plan.seats);
-
       if (plan.filled_seats && plan.seats) {
         const percentage = Math.round(((plan.filled_seats / plan.seats) * 100));
         core.setOutput('percentage', percentage);
