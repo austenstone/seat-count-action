1eca3aa8025cc0feb78c48f8e6a327ff86610242
Austen Stone <austenstone@github.com>
2023-08-16 19:17:06 -0400

    cleanup

--- src/run.ts
+++ src/run.ts
@@ -7,58 +7,48 @@ interface Input {
 }
 
 export function getInputs(): Input {
-  const result = {} as Input;
-  result.token = core.getInput('github-token');
-  result.org = core.getInput('org');
-  return result;
+  return {
+    token: core.getInput('github-token'),
+    org: core.getInput('org'),
+  };
 }
 
 const run = async (): Promise<void> => {
-  try {
-    const input = getInputs();
-    const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
+  const input = getInputs();
+  const octokit: ReturnType<typeof github.getOctokit> = github.getOctokit(input.token);
 
-    let plan;
-    if (github.context.serverUrl.match(/http[s]?:\/\/github.com/g)) {
-      const orgResponse = await octokit.request(`GET /orgs/${input.org}`);
-      core.debug(JSON.stringify({orgResponse}))
-      plan = orgResponse.data.plan;
-    } else {
-      const entResponse = await octokit.request(`GET /enterprise/settings/license`);
-      core.debug(JSON.stringify({entResponse}))
-      plan = {
-        seats: entResponse.data.seats,
-        filled_seats: entResponse.data.seats_used,
-      }
+  let plan;
+  let isDotCom = github.context.serverUrl.match(/http[s]?:\/\/github.com/g);
+  if (isDotCom) {
+    const orgResponse = await octokit.rest.orgs.get({ org: input.org });
+    core.debug(JSON.stringify({ orgResponse }))
+    plan = orgResponse.data.plan;
+  } else {
+    const entResponse = await octokit.request(`GET /enterprise/settings/license`);
+    core.debug(JSON.stringify({ entResponse }))
+    plan = {
+      seats: entResponse.data.seats,
+      filled_seats: entResponse.data.seats_used,
     }
+  }
 
-    core.debug(JSON.stringify({plan}))
-    if (plan) {
-      core.setOutput('name', plan.name);
-      core.setOutput('space', plan.space);
-      core.setOutput('private_repos', plan.private_repos);
-      core.setOutput('filled_seats', plan.filled_seats);
-      core.setOutput('seats', plan.seats);
-
-      if (plan.filled_seats && plan.seats) {
-        if (plan.seats === 'unlimited') {
-          core.setOutput('percentage', 0);
-          core.setOutput('remaining', 'unlimited');
-        } else {
-          const percentage = Math.round(((plan.filled_seats / plan.seats) * 100));
-          core.setOutput('percentage', percentage);
-          core.info(`${percentage}% of seats used`)
-          const remaining = plan.seats - plan.filled_seats;
-          core.setOutput('remaining', remaining);
-          core.info(`${remaining} seats remaining`)
-        }
+  core.debug(JSON.stringify({ plan }))
+  if (plan) {
+    Object.keys(plan).forEach(key => core.setOutput(key, plan[key]));
+    if (plan.filled_seats && plan.seats) {
+      let percentage = 0, remaining = 0;
+      if (plan.seats === 'unlimited') {
+        core.setOutput('remaining', 'unlimited');
+        percentage = 0;
+      } else {
+        const percentage = Math.round(((plan.filled_seats / plan.seats) * 100));
+        core.info(`${percentage}% of seats used`)
+        const remaining = plan.seats - plan.filled_seats;
+        core.info(`${remaining} seats remaining`)
       }
+      core.setOutput('percentage', percentage);
+      core.setOutput('remaining', remaining);
     }
-  } catch (error) {
-    core.setFailed(error instanceof Error ? error.message : JSON.stringify(error));
-    core.startGroup('Error');
-    core.error(JSON.stringify(error, null, 2));
-    core.endGroup();
   }
 };
 
