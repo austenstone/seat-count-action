e59825b3b0825a33e890fba7601ff5c1f42685be
Austen Stone <austenstone@github.com>
2024-05-12 12:40:53 -0400

    chore: Refactor run.ts for improved readability and maintainability

--- src/run.ts
+++ src/run.ts
@@ -22,25 +22,25 @@ const run = async (): Promise<void> => {
   let plan;
   const isDotCom = github.context.serverUrl.match(/http[s]?:\/\/github.com/g);
   if (input.enterprise) {
-    const entResponse = await octokit.request(`GET /enterprises/${input.enterprise}/consumed-licenses`);
+    const response = await octokit.request(`GET /enterprises/${input.enterprise}/consumed-licenses`);
     plan = {
-      filled_seats: entResponse.data.total_seats_consumed,
-      seats: entResponse.data.total_seats_purchased
+      filled_seats: response.data.total_seats_consumed,
+      seats: response.data.total_seats_purchased
     };
   } else if (isDotCom) {
-    const orgResponse = await octokit.rest.orgs.get({ org: input.org });
-    core.debug(JSON.stringify({ orgResponse }))
-    plan = orgResponse.data.plan;
+    const response = await octokit.rest.orgs.get({ org: input.org });
+    core.debug(JSON.stringify({ response }))
+    plan = response.data.plan;
   } else {
-    const entResponse = await octokit.request(`GET /enterprise/settings/license`);
-    core.debug(JSON.stringify({ entResponse }))
+    const response = await octokit.request(`GET /enterprise/settings/license`);
+    core.debug(JSON.stringify({ response }))
     plan = {
-      seats: entResponse.data.seats,
-      filled_seats: entResponse.data.seats_used,
+      filled_seats: response.data.seats_used,
+      seats: response.data.seats
     }
   }
 
-  core.debug(JSON.stringify({ plan }))
+  core.debug(JSON.stringify(plan, null, 2))
   if (plan) {
     Object.keys(plan).forEach(key => core.setOutput(key, plan[key]));
     if (plan.filled_seats && plan.seats) {
